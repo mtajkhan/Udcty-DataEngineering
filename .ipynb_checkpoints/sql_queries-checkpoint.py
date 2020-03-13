8d3cb947ceda8694321181425d1c2e6ab5d244ca
Mustafa Tajkhan <mtajkhan@yahoo.co.in>
2020-03-13 16:42:03 +0000

    Initial Commit

--- .ipynb_checkpoints/sql_queries-checkpoint.py
+++ .ipynb_checkpoints/sql_queries-checkpoint.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS tb_songplay"
+user_table_drop = "DROP TABLE IF EXISTS tb_user"
+song_table_drop = "DROP TABLE IF EXISTS tb_song"
+artist_table_drop = "DROP TABLE IF EXISTS tb_artist"
+time_table_drop = "DROP TABLE IF EXISTS tb_time"
 
 # CREATE TABLES
 
@@ -14,11 +14,10 @@ songplay_table_create = ("""
 user_table_create = ("""
 """)
 
-song_table_create = ("""
-""")
 
-artist_table_create = ("""
-""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS tb_song (song_id varchar,title varchar,artist_id varchar, year int, duration float);""")
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS tb_artist (artist_id varchar, artist_name varchar, artist_location varchar, artist_latitude varchar,  artist_longitude varchar);""")
 
 time_table_create = ("""
 """)
@@ -31,11 +30,9 @@ songplay_table_insert = ("""
 user_table_insert = ("""
 """)
 
-song_table_insert = ("""
-""")
+song_table_insert = ("""INSERT INTO tb_song (song_id,title,artist_id,year, duration) VALUES (%s, %s, %s, %s, %s)""")
 
-artist_table_insert = ("""
-""")
+artist_table_insert = ("""INSERT INTO tb_artist (artist_id,artist_name,artist_location,artist_latitude,artist_longitude) VALUES (%s, %s, %s, %s, %s)""")
 
 
 time_table_insert = ("""
@@ -48,5 +45,9 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+# create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
+create_table_queries = [song_table_create, artist_table_create]
+drop_table_queries = [song_table_drop, artist_table_drop]
+
